cabe497c951575b1aae7fc9d4af7e17ca3c50190
Azamorano8228 <zamoranoandres8228@gmail.com>
2025-12-04 11:20:15 -0800

    Initial Commit

---
+++ You Are Awesome/You_Are_AwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  You_Are_AwesomeApp.swift
+//  You Are Awesome
+//
+//  Created by ZAMORANO, ANDRES on 12/4/25.
+//
+
+import SwiftUI
+
+@main
+struct You_Are_AwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
